b2086175a527518742748bc4db1482db6a503ade
justin-hill426 <jrhill426@gmail.com>
2021-08-05 09:37:53 -0700

    Added new templating files

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return  render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/add-pet')
+def add_pet():
+    return render_template('addpet.html')
 
 
 if __name__ == "__main__":
