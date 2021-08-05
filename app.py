4a468054223e918a0cf237c890cab6874ae880a7
justin-hill426 <jrhill426@gmail.com>
2021-08-05 16:09:18 -0700

    Added template inheritance components

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -8,9 +8,15 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/add-pet')
+@app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
+    print(request.form)
     return render_template('addpet.html')
+
+
+@app.route('/pet')
+def pet():
+    return render_template('pet.html')
 
 
 if __name__ == "__main__":
